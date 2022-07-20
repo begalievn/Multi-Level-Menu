03e11ff2030197209a02e4bdc34f564370235337
begalievn <begaliev.info@gmail.com>
2022-07-20 11:20:20 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+import MultiMenus from './MultiMenus';
+
+const menus = [
+  {
+    label: 'Menu 1',
+  },
+  {
+    label: 'Menu 2',
+    submenu: [
+      {
+        label: 'Sub Menu 1',
+      },
+      {
+        label: 'Sub Menu 2',
+      },
+    ],
+  },
+  {
+    label: 'Menu 3',
+    submenu: [
+      {
+        label: 'Sub Menu 1',
+        submenu: [
+          {
+            label: 'Boom 1',
+          },
+          {
+            label: 'Boom 2',
+          },
+        ],
+      },
+      {
+        label: 'Sub Menu 2',
+        submenu: [
+          {
+            label: 'Deep 1',
+          },
+          {
+            label: 'Deep 2',
+            submenu: [
+              {
+                label: 'Lorem 1',
+              },
+              {
+                label: 'Lorem 2',
+                submenu: [
+                  {
+                    label: 'Super Deep',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Sub Menu 3',
+      },
+      {
+        label: 'Sub Menu 4',
+        submenu: [
+          {
+            label: 'Last 1',
+          },
+          {
+            label: 'Last 2',
+          },
+          {
+            label: 'Last 3',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Menu 4',
+  },
+];
 
 function App() {
+  const Wrapper = styled.aside`
+    background: #ccc;
+    width: 300px;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <MultiMenus menus={menus} />
+    </Wrapper>
   );
 }
 
